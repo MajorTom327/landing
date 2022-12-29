8753e0f75f7421cbe30909bf83515bef28862962
Valentin Thomas <me@valentin-thomas.com>
2022-12-29 07:45:12 +0100

    Linkedin url

--- app/refs/constants.ts
+++ app/refs/constants.ts
@@ -1,4 +1,4 @@
-export const linkedinUrl = "https://www.linkedin.com/in/%F0%9F%91%A8%F0%9F%8F%BC%E2%80%8D%F0%9F%92%BB-valentin-thomas-110236144";
+export const linkedinUrl = "https://www.linkedin.com/in/valentin-thomas-110236144/";
 export const twitterUrl = "https://twitter.com/MajorTom327";
 export const githubUrl = "https://github.com/MajorTom327";
 
