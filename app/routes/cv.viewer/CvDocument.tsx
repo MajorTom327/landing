830fb242cd571f87da65489e989736ea59377536
MajorTom327 <me@valentin-thomas.com>
2023-02-24 00:22:55 +0100

    CV Documents information

--- app/routes/cv.viewer/CvDocument.tsx
+++ app/routes/cv.viewer/CvDocument.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 import { Document } from '@react-pdf/renderer';
 import Page1 from './Page1';
 import Page2 from './Page2';
+import { useTranslation } from 'react-i18next';
 
 type Props = {
 };
 
 export const CvDocument: React.FC<Props> = ({}) => {
+  const { i18n } = useTranslation();
   return (<>
-    <Document>
+    <Document
+      title="CV Valentin THOMAS"
+      author='Valentin THOMAS'
+      language={i18n.language}
+      creator="Valentin THOMAS"
+      producer="Valentin THOMAS"
+      >
       <Page1 />
       <Page2 />
     </Document>
