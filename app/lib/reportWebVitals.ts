6e7f233232fa66288ef2f91e2f09881f3446b505
MajorTom327 <me@valentin-thomas.com>
2023-03-22 13:58:19 +0100

    Vitals

--- app/lib/reportWebVitals.ts
+++ app/lib/reportWebVitals.ts
@@ -1,12 +1,12 @@
-import { getCLS, getFID, getFCP, getLCP, getTTFB } from ("web-vitals")
+import { getCLS, getFID, getFCP, getLCP, getTTFB } from "web-vitals";
 
 const reportWebVitals = (onPerfEntry: any) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
+    getCLS(onPerfEntry);
+    getFID(onPerfEntry);
+    getFCP(onPerfEntry);
+    getLCP(onPerfEntry);
+    getTTFB(onPerfEntry);
   }
 };
 
