f81e324697f592536e0f3851a2a5dd2777203610
majortom327 <me@valentin-thomas.com>
2023-05-28 18:07:35 +0200

    translation cv review

--- app/routes/cv[.pdf]/index.tsx
+++ app/routes/cv[.pdf]/index.tsx
@@ -4,16 +4,16 @@ import CvDocument from "./CvDocument";
 import { pdf } from "remix-utils";
 import path from "node:path";
 
+
+
 import { i18n, supportedLanguages } from "~/services/i18n.server";
 import { I18nextProvider, initReactI18next } from "react-i18next";
 import i18next from "i18next";
 
 import LanguageDetector from "i18next-browser-languagedetector";
-import ChainedBackend from "i18next-chained-backend";
 
-import FsBackend from "i18next-fs-backend";
-
-const backends = [FsBackend];
+import translationEn from "../../../public/locales/en/cv.json";
+import translationFr from "../../../public/locales/fr/cv.json";
 
 export const loader: LoaderFunction = async ({ request }) => {
   const locale = await i18n.getLocale(request);
@@ -29,21 +29,15 @@ export const loader: LoaderFunction = async ({ request }) => {
   return i18next
     .use(initReactI18next)
     .use(LanguageDetector)
-    .use(ChainedBackend)
     .init({
       supportedLngs: supportedLanguages,
       lng: locale,
       fallbackLng: "fr",
       defaultNS: "translation",
       react: { useSuspense: true },
-      ns: ["translation", "cv"],
-      backend: {
-        backends,
-        backendOptions: [
-          {
-            loadPath,
-          },
-        ],
+      resources: {
+        en: { cv: translationEn },
+        fr: { cv: translationFr },
       },
       detection: {
         order: ["htmlTag"],
