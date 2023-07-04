e53c958942c60995b862e5ffdcd5252a8462f06c
majortom327 <me@valentin-thomas.com>
2023-07-04 17:14:52 +0200

    Fix refresh

--- remix.config.js
+++ remix.config.js
@@ -5,13 +5,14 @@ module.exports = {
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
-  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
-  serverBuildPath: "api/index.js",
+  serverModuleFormat: "cjs",
   tailwind: true,
   future: {
     v2_errorBoundary: true,
-    v2_normalizeFormMethod: true,
     v2_meta: true,
+    v2_normalizeFormMethod: true,
     v2_routeConvention: true,
+    v2_headers: true,
+    v2_dev: true,
   },
 };
