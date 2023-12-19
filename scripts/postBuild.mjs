77d17f55466687ded5969a5f5ebd391fc814377f
majortom327 <me@valentin-thomas.com>
2023-12-19 00:25:40 -0800

    Update postBuild script to use Node.js module

---
+++ scripts/postBuild.mjs
@@ -0,0 +1,20 @@
+import fs from "fs";
+import path from "path";
+
+const key = /BUILD_DATE/g;
+const date = new Date().toISOString();
+
+(() => {
+  const dir = path.join(__dirname, "../dist");
+
+  fs.readdirSync(dir).forEach((file) => {
+    const filePath = path.join(dir, file);
+    const stat = fs.statSync(filePath);
+
+    if (stat.isFile()) {
+      const content = fs.readFileSync(filePath, "utf8");
+      const result = content.replace(key, date);
+      fs.writeFileSync(filePath, result, "utf8");
+    }
+  });
+})();
