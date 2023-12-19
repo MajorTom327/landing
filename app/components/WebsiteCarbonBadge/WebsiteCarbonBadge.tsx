db45f4ab1b496610fd2f33f53310c9badde7eac2
majortom327 <me@valentin-thomas.com>
2023-12-19 02:02:15 -0800

    Fix import and uncomment CarbonBadge component

--- app/components/WebsiteCarbonBadge/WebsiteCarbonBadge.tsx
+++ app/components/WebsiteCarbonBadge/WebsiteCarbonBadge.tsx
@@ -1,11 +1,10 @@
-import type React from "react";
-
-// import CarbonBadge from "react-carbonbadge";
+import React from "react";
+import CarbonBadge from "react-carbonbadge";
 
 // Bug: Cannot load the CarbonBadge component from react-carbonbadge and render it
 export const WebsiteCarbonBadge: React.FC = () => {
   return null;
-  // return <CarbonBadge darkMode={true} />;
+  return <CarbonBadge darkMode={true} />;
 };
 
 export default WebsiteCarbonBadge;
